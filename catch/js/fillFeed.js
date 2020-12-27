697d7a080aae7a4c31e06ddb3c97d007ce9f8ece
Fabian <lindgrenfabian@gmail.com>
2020-12-27 20:49:11 +0100

    remove waiting for feed

--- catch/js/fillFeed.js
+++ catch/js/fillFeed.js
@@ -1,23 +1,22 @@
-$(document).ready(function () {
-    //Creates cards for all beatmaps in database and post them to the front page on load.
+//Creates cards for all beatmaps in database and post them to the front page on load.
 
-    class bmap {
-        constructor(id, title, artist, difficulty, thumbnail, length, creator) {
-            this.id = id,
-                this.title = title,
-                this.artist = artist,
-                this.difficulty = difficulty,
-                this.thumbnail = thumbnail,
-                this.length = length,
-                this.creator = creator
-        }
+class bmap {
+    constructor(id, title, artist, difficulty, thumbnail, length, creator) {
+        this.id = id,
+            this.title = title,
+            this.artist = artist,
+            this.difficulty = difficulty,
+            this.thumbnail = thumbnail,
+            this.length = length,
+            this.creator = creator
+    }
 
-        get post() {
-            return this.generatePost();
-        }
+    get post() {
+        return this.generatePost();
+    }
 
-        generatePost() {
-            return `
+    generatePost() {
+        return `
         <li class="article_post">
     <a href='#' onclick='startGame("${this.thumbnail.replace("jpg", "osu").replace("png", "osu")}", "${this.thumbnail}", "${this.thumbnail.replace("jpg", "mp3").replace("png", "mp3")}")' class="inner">
       <div class="li-img">
@@ -31,44 +30,43 @@ $(document).ready(function () {
       </div>
     </a>
   </li>`;
-        }
+    }
+}
+
+
+//gets a list of all articles in database on page load
+var bmaps = [];
+$.post("/catch/getmaps", function (data) {
+    $.each(data, function (i, post) {
+        bmaps.push(new bmap(post.id, post.title, post.artist, post.difficulty, post.thumbnail, post.length, post.creator));
+    })
+    bmaps.reverse()
+});
+
+function UpdateFeed() {
+    $.each(bmaps, function (i, post) {
+        $(".img-list").append($(post.post));
+    })
+}
+
+function secondsToDisplay(duration) {
+    // Hours, minutes and seconds
+    var hrs = ~~(duration / 3600);
+    var mins = ~~((duration % 3600) / 60);
+    var secs = ~~duration % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
     }
 
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
 
-    //gets a list of all articles in database on page load
-    var bmaps = [];
-    $.post("/catch/getmaps", function (data) {
-        $.each(data, function (i, post) {
-            bmaps.push(new bmap(post.id, post.title, post.artist, post.difficulty, post.thumbnail, post.length, post.creator));
-        })
-        bmaps.reverse()
-    });
-
-    function UpdateFeed() {
-        $.each(bmaps, function (i, post) {
-            $(".img-list").append($(post.post));
-        })
-    }
-
-    function secondsToDisplay(duration) {
-        // Hours, minutes and seconds
-        var hrs = ~~(duration / 3600);
-        var mins = ~~((duration % 3600) / 60);
-        var secs = ~~duration % 60;
-
-        // Output like "1:01" or "4:03:59" or "123:03:59"
-        var ret = "";
-
-        if (hrs > 0) {
-            ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
-        }
-
-        ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-        ret += "" + secs;
-        return ret;
-    }
-
-    window.onload = function () {
-        UpdateFeed();
-    };
-})
+window.onload = function () {
+    UpdateFeed();
+};
