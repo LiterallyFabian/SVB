c45d4316b9bb7138e7a83f9fade98403c7f5e100
Fabian <lindgrenfabian@gmail.com>
2020-12-30 14:04:16 +0100

    update login host

--- js/navigationbar.js
+++ js/navigationbar.js
@@ -33,7 +33,7 @@ const Login = {
         return {
             navoption: "Logga in",
             username: "",
-            //dev: https://discord.com/api/oauth2/authorize?client_id=793179363029549057&redirect_uri=http%3A%2F%2F192.168.56.101%3A3000%2Fauth&response_type=code&scope=identify
+            //dev: https://discord.com/api/oauth2/authorize?client_id=793179363029549057&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth&response_type=code&scope=identify
             //prod: https://discord.com/api/oauth2/authorize?client_id=793179363029549057&redirect_uri=https%3A%2F%2Fsvt.sajber.me%2Fauth&response_type=code&scope=identify
             href: "https://discord.com/api/oauth2/authorize?client_id=793179363029549057&redirect_uri=https%3A%2F%2Fsvt.sajber.me%2Fauth&response_type=code&scope=identify",
             profilehref: "",
