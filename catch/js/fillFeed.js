030e949d048a718c618ea766d2d659182015eda8
Fabian <lindgrenfabian@gmail.com>
2021-05-09 14:54:52 +0200

    rewrote fillfeed method to use axios
    closes #9

--- catch/js/fillFeed.js
+++ catch/js/fillFeed.js
@@ -2,14 +2,18 @@
 class bmap {
     constructor(beatmap) {
         this.beatmap = beatmap;
-    }
+    };
 
     generatePost() {
+        var rankOverlay = ``;
+        var score = catchScores[this.beatmap.id];
+        if (score) rankOverlay = `<img class="rankOverlay" src="/img/ranking-${score.rank.toUpperCase()}.png">`;
+
         return `
         <li class="beatmapCard" id="card-${this.beatmap.id}">
             <figure> 
             <div class="parent">
-            <img class="rankOverlay" id="rank-${this.beatmap.id}">
+                ${rankOverlay}
                 <a onClick="playPreview('${this.beatmap.path}', '${this.beatmap.id}')"> <i id="icon-${this.beatmap.id}" title="Preview song" class="fas fa-play playButton"></i></a>
                 <a onclick='startID(${this.beatmap.id})' class="inner">
                 <img class="thumbnail" src="/${this.beatmap.path.replace("song/", "song/icon/")}.jpg" alt="thumbnail">
@@ -25,48 +29,33 @@ class bmap {
 }
 
 
-//gets a list of all articles in database on page load
-var bmaps = [];
 beatmapDatabase = {};
-$.post("/catch/getmaps", function (data) {
-    $.each(data, function (i, map) {
-        beatmapDatabase[map.id.toString()] = map;
-        bmaps.push(new bmap(map));
-    })
-    UpdateFeed();
-});
-
 catchScores = {}
-if (getCookie("auth").length > 0) {
-    $.post("/auth/getuser", {
-        auth: JSON.stringify(getAuth())
-    }, function (response) {
-        if (response) {
-             catchScores = JSON.parse(response[0].catchScores).ranks;
-            $.each(catchScores, function (i, score) {
-                if (score.id) $(`#rank-${score.id}`).attr("src", `/img/ranking-${score.rank.toUpperCase()}.png`);
-            })
-        }
-    });
-}
 
-function UpdateFeed() {
-    $.each(bmaps, function (i, post) {
-        $(`.image-list`).append($(post.generatePost()));
-        if (bmaps.length > 0) document.getElementById('no-map-alert').style.display = "none";
-    })
+axios.all([
+        axios.post("/catch/getmaps"),
+        axios.post("/auth/getuser", {
+            auth: getCookie("auth").length > 0 ? JSON.stringify(getAuth()) : -1
+        })
+    ])
+    .then(axios.spread((maps, ranks) => {
+        catchScores = JSON.parse(ranks.data[0].catchScores).ranks;
+        $.each(maps.data, function (i, map) {
+            beatmapDatabase[map.id.toString()] = map;
+            $(`.image-list`).append($(new bmap(map).generatePost()));
+        })
+        document.getElementById('no-map-alert').style.display = "none";
+        $(function () {
+            $(".beatmapCard").hover(
+                function () {
+                    if (hoverAudio.paused) hoverAudio.play();
+                    else hoverAudio.currentTime = 0
+                },
+                function () {
 
-    $(function () {
-        $(".beatmapCard").hover(
-            function () {
-                if (hoverAudio.paused) hoverAudio.play();
-                else hoverAudio.currentTime = 0
-            },
-            function () {
-
-            });
-    });
-}
+                });
+        });
+    }));
 
 //card hovering audio
 var hoverAudio = new Audio('/catch/audio/hover.ogg')
