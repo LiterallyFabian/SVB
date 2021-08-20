3a5d55dbcb989ea06e84ae780e12db33e8f0ceae
Fabian <lindgrenfabian@gmail.com>
2021-08-20 15:12:08 +0200

    add descriptions to skins

--- app/public/catch/js/skins.js
+++ app/public/catch/js/skins.js
@@ -1,36 +1,43 @@
 var skins = [{
     name: "Default (osu!catch)",
     id: "osu",
+    description: "Play with the classic fruits",
     status: "checked",
     hasDrop: false
 }, {
     name: "osu!",
     id: "circle",
+    description: "Catch the circles!",
     status: "",
     hasDrop: false
 }, {
     name: "Flowers",
     id: "flower",
+    description: "Catch some pretty flowers",
     status: "",
     hasDrop: false
 }, {
     name: "Kanna Kamui'",
     id: "kanna",
+    description: "Play with some basic but pretty circles",
     status: "",
     hasDrop: true
 }, {
     name: "American",
     id: "american",
+    description: "Why not catch some fast food?",
     status: "",
     hasDrop: false
 }, {
     name: "Hunie",
     id: "hunie",
+    description: "A different art style won't hurt",
     status: "",
     hasDrop: false
 }, {
     name: "Sarah",
     id: "sarah",
+    description: "SUGOIIIIII ~",
     status: "",
     hasDrop: false
 }]
@@ -54,7 +61,7 @@ function fillSkins() {
         <div class="skin-container">
             <div class="skin-label">
                 <input type="radio" class="skin-selector" id="${skin.id}" name="skin" value="${skin.id}" ${skin.status}>
-                <label for="${skin.id}">${skin.name}</label>
+                <label for="${skin.id}" title="${skin.description}">${skin.name}</label>
             </div>
             <img src="img/hitobjects/${skin.id}-1.png">
             <img src="img/hitobjects/${skin.id}-2.png">
